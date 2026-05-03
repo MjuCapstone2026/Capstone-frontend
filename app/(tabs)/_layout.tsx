612ee57f1d4cbba009afab8ef9adde629af9ec33
DOOYEE0709 <somewhere.dooyee@gmail.com>
2026-05-03 18:00:56 +0900

    chore(components): Expo 초기 템플릿 컴포넌트 제거 및 프로젝트 컴포넌트로 대체

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,14 +11,11 @@ import { ActivityIndicator, View } from 'react-native';
 
 // Clerk 인증 훅
 import { useAuth } from '@clerk/clerk-expo';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useTheme } from '@/hooks/useTheme';
+import IcHomeLabel from '@/assets/icons/ic_home_label.svg';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const { colors, scheme } = useTheme();
 
   /**
    * 인증 상태 확인
@@ -55,22 +52,14 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: colors.primaryLight,
         headerShown: false,
-        tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IcHomeLabel width={28} height={28} color={color} />,
         }}
       />
     </Tabs>
